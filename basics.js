291fa3e43cd4910a1865eb16495735628667e304
Terry Fadz <tfadz@suntimes.com>
2018-10-16 11:39:58 -0500

    add basics

---
+++ basics.js
@@ -0,0 +1,16 @@
+// Select class 
+var hektor = document.querySelector('.hektor');
+
+// Add class to div hektor
+hektor.classList.add("red");
+
+// Run through array and assign number value
+var blazer = ['dog', 'female', 'pointer'];
+
+for (let i = 0; i < blazer.length; i++) {
+  console.log(blazer[i]) //value
+  console.log(i) //index
+}
+
+
+console.log(hektor);
