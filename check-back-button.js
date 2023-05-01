27a741a6c8584b0a6d5c801c7953ab2b67c0e0dd
Terry Fadz <tfadz@users.noreply.github.com>
2023-05-01 15:05:42 -0500

    Updated code.

--- check-back-button.js
+++ check-back-button.js
@@ -1,12 +1,10 @@
 jQuery(function($) {
 	if(!!window.performance && window.performance.navigation.type === 2) {
-		window.setTimeout(function() { 
-			$('.facetwp-type-checkboxes .facetwp-checkbox .facetwp-display-value').each(function() {
-				var $captureCheckboxText = $(this).text();
-				if($captureCheckboxText == '<?php echo $customTaxName ?>') {
-					$(this).parent().trigger("click");
-				}
-			});
-		}, 100);
+		$('.facetwp-type-checkboxes .facetwp-checkbox .facetwp-display-value').each(function() {
+			var $captureCheckboxText = $(this).text();
+			if($captureCheckboxText == '<?php echo $customTaxName ?>') {
+				$(this).parent().trigger("click");
+			}
+		});
 	}
 });
